c000c5ff3fa7bd7437cdd1a73bf7d1d7feded7ed
Doris8462 <liandou123>
2020-09-11 08:16:25 +0800

    [huangyunjie] feat:complete practice1

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,15 @@
-const parseData
+const parseData = (input) => {
+    const { data, column } = input;
+    const columnName = column.map(item => item.name)
+    const result = []
+    data.map(person => {
+        const tempt = {}
+        for (let inedx in columnName) {
+            tempt[columnName[inedx]] = person[inedx]
+        }
+        result.push(tempt)
+    })
+    return result
+};
+
 export { parseData };
