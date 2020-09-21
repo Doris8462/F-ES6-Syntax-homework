bcc4e77547aef41975f5810214993292802ed123
kevindongzg <dongkevin94@gmail.com>
2020-09-21 23:12:29 +0800

    add TODO

--- src/practice1.js
+++ src/practice1.js
@@ -1,9 +1,13 @@
 const parseData = (input) => {
+    // TODO 解构可以优化到函数接受参数中
     const { data, column } = input;
+    // TODO columnName 没有必要
     const columnName = column.map(item => item.name)
+    // TODO 使用map做数组与数组的转化，不用声明result，查查map的使用
     const result = []
     data.map(person => {
         const tempt = {}
+        // TODO index拼写错误
         for (let inedx in columnName) {
             tempt[columnName[inedx]] = person[inedx]
         }
--- src/practice2.js
+++ src/practice2.js
@@ -1,4 +1,6 @@
+// TODO 逻辑纰漏，当sections内items顺序不是index顺序时会有问题
 const inject = (items, sections) => {
+    // TODO 不是map的使用场景
     sections.map(section => {
         items.splice(sections.indexOf(section) + section.index, 0, section.content)
     })
