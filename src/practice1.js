f41c235f02f18216c5fcfddb6239dd8ca0a498d7
Doris8462 <liandou123>
2020-09-22 09:58:48 +0800

    [huangyunjie] fix

--- src/practice1.js
+++ src/practice1.js
@@ -1,19 +1,16 @@
-const parseData = (input) => {
+const parseData = ({ data, column }) => {
     // TODO 解构可以优化到函数接受参数中
-    const { data, column } = input;
     // TODO columnName 没有必要
     const columnName = column.map(item => item.name)
     // TODO 使用map做数组与数组的转化，不用声明result，查查map的使用
-    const result = []
-    data.map(person => {
+    return data.map(person => {
         const tempt = {}
         // TODO index拼写错误
-        for (let inedx in columnName) {
-            tempt[columnName[inedx]] = person[inedx]
+        for (let index in columnName) {
+            tempt[columnName[index]] = person[index]
         }
-        result.push(tempt)
+        return tempt
     })
-    return result
 };
 
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,9 +1,23 @@
 // TODO 逻辑纰漏，当sections内items顺序不是index顺序时会有问题
 const inject = (items, sections) => {
-    // TODO 不是map的使用场景
-    sections.map(section => {
-        items.splice(sections.indexOf(section) + section.index, 0, section.content)
-    })
-    return items;
-}
+  function compare(propertyName) {
+    return function (object1, object2) {
+      var value1 = object1[propertyName];
+      var value2 = object2[propertyName];
+      if (value2 < value1) {
+        return 1;
+      } else if (value2 > value1) {
+        return -1;
+      } else {
+        return 0;
+      }
+    };
+  }
+  sections.sort(compare("index"));
+  // TODO 不是map的使用场景
+  sections.forEach((section) => {
+    items.splice(sections.indexOf(section) + section.index, 0, section.content);
+  });
+  return items;
+};
 export { inject };
